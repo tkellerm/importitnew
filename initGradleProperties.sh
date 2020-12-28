41f24f01d229838b293d287011df0a61e557f7f8
Tobias Kellermann <tobias.kellermann@abas.de>
2020-12-28 11:43:26 +0100

    IN-680 start with the bitbucket pipeline

--- initGradleProperties.sh
+++ initGradleProperties.sh
@@ -1,16 +1,58 @@
 #!/bin/bash
 
-if [ Msys = '$(uname -o)2>/dev/null' ]; then #git for windows
-	DOCKER_HOST=$(docker-machine ip default)
-elif [ Darwin = "$(uname -s)" ]; then #mac
-	DOCKER_HOST=$(docker-machine ip default)
-else #other
-	DOCKER_HOST=$(hostname)
+if [ ! $DOCKER_ENV_HOST ]; then
+    if [ Msys = '$(uname -o)2>/dev/null' ]; then #git for windows
+        DOCKER_ENV_HOST=$(docker-machine ip default)
+    else #other
+        DOCKER_ENV_HOST=$(hostname)
+    fi
 fi
+
+if [[ $BITBUCKET_BRANCH == *"2019"* ]]; then
+  export ABAS_VERSION=2019
+  export USE_ERP_VERSION=2019r4n18p01
+fi
+
+if [[ $BITBUCKET_BRANCH == *"2018"* ]]; then
+  export ABAS_VERSION=2018
+  export USE_ERP_VERSION="2018r4n14p42"
+fi
+
+if [[ $BITBUCKET_BRANCH == *"2017"* ]]; then
+  export ABAS_VERSION=2017
+  export USE_ERP_VERSION="2017r4n16p39"
+fi
+
+sed "s+2019r4n18p01+$USE_ERP_VERSION+g" -i "./Dockerfile-erp-overrides"
+
+echo ABAS_VERSION ${ABAS_VERSION}
+echo USE_ERP_VERSION ${USE_ERP_VERSION}
+
+if [[ $BITBUCKET_BRANCH == *"feature"* ]]; then
+  export SNAPSHOT="-SNAPSHOT"
+else
+  export SNAPSHOT=""
+fi
+
+
+if [ -f version ]; then
+      VERSION=$(cat version)
+      export APP_VERSION=${VERSION}.${ABAS_VERSION}.${BITBUCKET_BUILD_NUMBER}
+else
+      export APP_VERSION=1.${ABAS_VERSION}.${BITBUCKET_BUILD_NUMBER}
+fi
+echo $APP_VERSION
+
+if [ ! $APP_VERSION ]; then
+
+  exit 1 "APP_VERSION not set"
+fi
+
+
 pwd=$(pwd)
-WF_URL="http://$DOCKER_HOST:8088/engine-rest"
 
 find . -name "*.properties.template" | while IFS= read -r pathname; do
     dirname=$(dirname "$pathname")
-    sed "s+DOCKER_HOST+$DOCKER_HOST+g; s+WF_URL+$WF_URL+g" "$pathname" > "$dirname/gradle.properties"
+    sed "s+DOCKER_ENV_HOST+$DOCKER_ENV_HOST+g" "$pathname" > "$dirname/gradle.properties"
+    sed "s+APP_VERSION+$APP_VERSION$SNAPSHOT+g" -i "$dirname/gradle.properties"
 done
